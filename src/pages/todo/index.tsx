305bedf63b606e096f93989dbbf5500029506517
Patrick Cruz <patrickezequielscruz@gmail.com>
2023-10-29 12:36:41 +0100

    prettier test

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -3,28 +3,29 @@ import styles from "./index.module.scss";
 import { useEffect, useState } from "react";
 
 export default function todo() {
-    const [dataList, setDataList]: any = useState([]);
+  const [dataList, setDataList]: any = useState([]);
 
-    useEffect(() => {
-        fetchTasks()
-            .then(dataList => {
-                setDataList(dataList);
-            })
-            .catch(error => {
-                // Handle errors if needed
-                console.error(error);
-            });
-    }, []);
+  useEffect(() => {
+    fetchTasks()
+      .then((dataList) => {
+        setDataList(dataList);
+      })
+      .catch((error) => {
+        // Handle errors if needed
+        console.error(error);
+      });
+  }, []);
 
-    console.log(`data `, dataList)
-    return (
-        <>
-        <div className={styles.container}>
-          {dataList.map((element: any, index: number) => (
-            <p key={index}>{element.title}</p>
-          ))}
-        </div>
-        </>
-        );
+
+  console.log(`data `, dataList) ;
+  return ( 
+
+    < >
+      <div className={styles.container}>
+        {dataList.map((element: any, index: number) => (
+          <p key={index}>{element.title}</p>
+        ))}
+      </div>
+    </>
+  );
 }
- 
